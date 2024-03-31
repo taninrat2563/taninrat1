273796bf76047d5b242e135aad28ad4dee3d4744
taninrat2563@ <taninrat2563@gmail.com>
2024-03-31 21:57:20 +0700

    My first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>สวัสดีตอนเช้า</title>
+</head>
+<body>
+    <?php
+        date_default_timezone_set('Asia/Bangkok');
+        $time = date("H");
+
+        if ($time < "12") {
+            echo "<h1>สวัสดีตอนเช้า</h1>";
+        } else {
+            echo "<h1>สวัสดีตอนบ่าย</h1>";
+        }
+    ?>
+</body>
+</html>
